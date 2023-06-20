55ec739fa2dd644aeecd10449a154973cc9e2d42
Guilherme Coelho <guilherme.coelho@veredaeducacao.com>
2023-06-20 10:54:46 -0300

    feat: create file generator for useQuery

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,10 +1,10 @@
 export default function (plop) {
-  plop.setGenerator("component", {
-    description: "application controller logic",
+  plop.setGenerator("create-component", {
+    description: "Create react component with tests and storybook",
     prompts: [
       {
         type: "input",
-        name: "name",
+        name: "component",
         message: "Component name:",
       },
       {
@@ -12,13 +12,55 @@ export default function (plop) {
         name: "path",
         message: "Add relative path:",
       },
+      {
+        type: "input",
+        name: "storieTitle",
+        message: "Add storie title:",
+      },
     ],
     actions: [
       {
         type: "add",
-        path: "../{{path}}/{{pascalCase name}}.tsx",
-        templateFile: "../generators/templates/component.tsx.hbs",
+        path: "../{{path}}/{{pascalCase component}}/index.tsx",
+        templateFile: "./templates/createComponent/component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../{{path}}/{{pascalCase component}}/style.ts",
+        templateFile: "./templates/createComponent/style.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "../{{path}}/{{ pascalCase component }}/storybook/{{pascalCase component}}.stories.tsx",
+        templateFile: "./templates/createComponent/stories.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../{{path}}/{{ pascalCase component}}/__tests__/{{ pascalCase component}}.spec.tsx",
+        templateFile: "./templates/createComponent/test.tsx.hbs",
       },
     ],
-  });
+  }),
+    plop.setGenerator("react-query/useQuery", {
+      description: "Create custom hook for useQuery hook",
+      prompts: [
+        {
+          type: "input",
+          name: "queryName",
+          message: "Add query name:",
+        },
+        {
+          type: "input",
+          name: "path",
+          message: "Add relative path:",
+        },
+      ],
+      actions: [
+        {
+          type: "add",
+          path: "../{{path}}/{{ camelCase queryName }}/index.ts",
+          templateFile: "./templates/reactQuery/useQuery.ts.hbs",
+        },
+      ],
+    });
 }
